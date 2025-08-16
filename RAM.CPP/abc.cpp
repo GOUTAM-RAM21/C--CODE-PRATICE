3f4357c996eb3a800075739fa3789113abd72fb9
Goutam <gautaminda3547@gmail.com>
2025-08-16 22:47:37 +0530

    git commit -m "Add solution for different neighbours problem"

---
+++ RAM.CPP/abc.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int n = (int)s.length();
+    int count = 0;
+
+    
+    for (int i = 0; i < n; i++) {
+
+        if (i == 0) {
+            
+            if (n >= 2 && s[i] != s[i + 1]) count++;
+        }
+        else if (i == n - 1) {
+            
+            if (s[i] != s[i - 1]) count++;
+        }
+        else {
+            
+            if (s[i] != s[i - 1] && s[i] != s[i + 1]) count++;
+        }
+    }
+
+    cout << count;
+    return 0;
+}
